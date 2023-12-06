2e99d1e0be12ad128e798406ff638bcc01f39673
NamKiHyeok <carpediem7490@gmail.com>
2023-12-06 12:40:29 +0900

    2023.12.06 feat: Vue Router 추가,실습(로그인화면)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,18 @@
 <template>
  <Header/>
- <Home/>
+ <RouterView/>
  <Footer/>
 </template>
 
 <script>
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-import Home from "@/pages/Home"
 
 export default {
   name: 'App',
   components: {
     Header,
     Footer,
-    Home
   }
 }
 </script>
